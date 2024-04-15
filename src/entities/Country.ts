ceb0793f12cfe5d81f3c1f423e02fb0124410edc
Aurelie Menin <atypik77@hotmail.fr>
2024-04-15 17:30:31 +0200

    Add resolvers & entities

--- src/entities/Country.ts
+++ src/entities/Country.ts
@@ -1,5 +1,6 @@
 import { Field, ID, ObjectType } from "type-graphql";
-import { BaseEntity, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { CreateOrUpdateCountry } from "./Country.args";
 
 @Entity()
 @ObjectType()
@@ -7,4 +8,74 @@ export class Country extends BaseEntity {
   @PrimaryGeneratedColumn()
   @Field(() => ID)
   id!: number;
+
+  @Column()
+  @Field()
+  name!: string;
+
+  @Column()
+  @Field()
+  code!: string;
+
+  @Column()
+  @Field()
+  emoji!: string;
+
+  @Column()
+  @Field()
+  continentCode!: string;
+
+  constructor(country?: CreateOrUpdateCountry) {
+    super();
+    if (country) {
+      if (!country.name) {
+        throw new Error("Country name cannot be empty.");
+      }
+      this.name = country.name;
+
+      if (!country.code) {
+        throw new Error("Country code cannot be empty.");
+      }
+      this.code = country.code;
+
+      if (!country.emoji) {
+        throw new Error("Country emoji cannot be empty.");
+      }
+      this.emoji = country.emoji;
+
+      if (!country.continentCode) {
+        throw new Error("Country continentCode cannot be empty.");
+      }
+      this.continentCode = country.continentCode;
+    }
+  }
+
+  static async getCountries(): Promise<Country[]> {
+    return await Country.find();
+  }
+
+  static async getCountriesByContinentCode(
+    continentCode: string
+  ): Promise<Country[]> {
+    const countries = await Country.findBy({ continentCode });
+    if (countries.length === 0) {
+      throw new Error(
+        `No countries found for continent with the code: ${continentCode}.`
+      );
+    }
+    return countries;
+  }
+
+  static async getCountryByCode(code: string): Promise<Country> {
+    const country = await Country.findOneBy({ code });
+    if (!country) {
+      throw new Error(`The country does not exist with the code: ${code}.`);
+    }
+    return country;
+  }
+
+  static async saveNewCountry(countryData: CreateOrUpdateCountry) {
+    const newCountry = new Country(countryData);
+    return await newCountry.save();
+  }
 }
--- src/resolvers/CountryResolvers.ts
+++ src/resolvers/CountryResolvers.ts
@@ -1,10 +1,27 @@
 import { Country } from "../entities/Country";
-import { Query, Resolver } from "type-graphql";
+import { Arg, Args, Mutation, Query, Resolver } from "type-graphql";
+import { CreateOrUpdateCountry } from "../entities/Country.args";
 
 @Resolver(Country)
 export class CountryResolvers {
+  @Mutation(() => Country)
+  createCountry(@Args() args: CreateOrUpdateCountry) {
+    return Country.saveNewCountry(args);
+  }
   @Query(() => [Country])
   countries() {
-    return Country.find();
+    return Country.getCountries();
+  }
+
+  @Query(() => Country)
+  countryByCode(@Arg("code", () => String) code: string) {
+    return Country.getCountryByCode(code);
+  }
+
+  @Query(() => [Country])
+  countriesByContinentCode(
+    @Arg("continentCode", () => String) continentCode: string
+  ) {
+    return Country.getCountriesByContinentCode(continentCode);
   }
 }
